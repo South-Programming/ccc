92defa2145ce597a3c19a14262ae26bb4d5d822c
Omar <61359709+omarabusharar@users.noreply.github.com>
2025-02-04 22:35:34 -0800

    Posted Solutions
    Feb 5 (Java) - Omar

---
+++ Feb 5 2025/CCC.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class CCC {
+    public static void main(String[] args) {
+       
+        // Create and initialize variables needed
+        Scanner input = new Scanner(System.in);
+        int value = input.nextInt();
+        int fourValue = value;
+        int fiveValue = value;
+        int mixedValue = value;
+        int ways = 0;
+        
+        // How many fours until zero?
+        while(fourValue > 0) {
+            fourValue -= 4;
+            if (fourValue == 0) {
+                ways += 1;
+            }
+        }
+
+         // How many fives until zero?
+        while(fiveValue > 0) {
+            fiveValue -= 5;
+            if (fiveValue == 0) {
+                ways += 1;
+            }
+        }
+
+        // How many fours and fives until zero?
+        while (mixedValue > 0) {
+            if (mixedValue % 4 == 0) {
+                mixedValue -= 4;
+            } else {
+                mixedValue -= 5;
+            }
+            if (mixedValue == 0) {
+                ways += 1;
+            }
+        }
+
+
+        // Output Result (Total Ways)
+        System.out.println(ways);
+    }
+}
